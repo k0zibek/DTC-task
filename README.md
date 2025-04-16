571ccf36f36c04c9dc2f4cad6a7c64e78e93159e
Kazi <uidboy01@gmail.com>
2025-04-16 15:22:02 +0500

    inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# News app
+## React/React Native + Typescript + Vite
---
+++ web-app/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import * as path from 'path';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslint(), svgr({
+    include: "**/*.svg?react",
+  }), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@public': path.resolve(__dirname, 'public/'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
+  build: {
+    sourcemap: true
+  }
+})
