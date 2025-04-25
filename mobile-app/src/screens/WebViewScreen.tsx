7d10fab5824a24986b2e40db0af936de4c896e93
Kazi <uidboy01@gmail.com>
2025-04-25 19:45:22 +0500

    fix: webview open link in browser

--- mobile-app/src/screens/WebViewScreen.tsx
+++ mobile-app/src/screens/WebViewScreen.tsx
@@ -1,24 +1,60 @@
-import { Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import * as Linking from "expo-linking";
+import { BackHandler, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { useAuthStore } from "../services/auth/model/authStore";
+import { useEffect, useRef, useState } from "react";
+
+const startURI = 'http://192.168.100.61:5173/'
 
 export const WebViewScreen = ({ navigation }: any) => {
+  const webViewRef = useRef<WebView>(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  const handleLogout = () => {
+    useAuthStore.persist.clearStorage();
+    useAuthStore.getState().logout();
+    navigation.replace("Auth");
+  };
+
+  // Обработка аппаратной кнопки "назад" на Android
+  useEffect(() => {
+    const onBackPress = () => {
+      if (canGoBack && webViewRef.current) {
+        webViewRef.current.goBack();
+        return true;
+      }
+      return false;
+    };
+
+    BackHandler.addEventListener("hardwareBackPress", onBackPress);
+    return () => BackHandler.removeEventListener("hardwareBackPress", onBackPress);
+  }, [canGoBack]);
+
   return (
       <View className="flex-1">
         <SafeAreaView className="flex-1">
           <WebView
-              source={{ uri: 'http://192.168.100.61:5173/' }}
+              ref={webViewRef}
+              source={{ uri: startURI }}
+              onNavigationStateChange={(navState) => {
+                setCanGoBack(navState.canGoBack);
+              }}
               originWhitelist={["*"]}
+              onShouldStartLoadWithRequest={(request) => {
+                if (request.url !== startURI) {
+                  Linking.openURL(request.url);
+                  return false;
+                }else {
+                  return true;
+                }
+              }}
               startInLoadingState
           />
         </SafeAreaView>
 
         <View className="mb-5">
           <TouchableOpacity
-              onPress={() => {
-                useAuthStore.persist.clearStorage();
-                navigation.replace("Auth")
-              }}
+              onPress={handleLogout}
               className="bg-blue-600 px-6 py-3 rounded-lg"
           >
             <Text className="text-white text-center font-semibold text-lg">Выйти</Text>
