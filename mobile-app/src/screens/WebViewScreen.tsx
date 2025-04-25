d60a158f92cc853763ed38e265f55e9857ea4ec0
Kazi <uidboy01@gmail.com>
2025-04-25 17:40:57 +0500

    fix: webview fixed

--- mobile-app/src/screens/WebViewScreen.tsx
+++ mobile-app/src/screens/WebViewScreen.tsx
@@ -1,18 +1,29 @@
-import { StyleSheet, View, SafeAreaView } from 'react-native';
+import { Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { useAuthStore } from "../services/auth/model/authStore";
 
-export const WebViewScreen = () => {
+export const WebViewScreen = ({ navigation }: any) => {
   return (
-      <View>
-        <SafeAreaView style={styles.safeAreaWrapper}>
-          <WebView source={{ uri: 'http://192.168.100.61:5173/' }} />
+      <View className="flex-1">
+        <SafeAreaView className="flex-1">
+          <WebView
+              source={{ uri: 'http://192.168.100.61:5173/' }}
+              originWhitelist={["*"]}
+              startInLoadingState
+          />
         </SafeAreaView>
+
+        <View className="mb-5">
+          <TouchableOpacity
+              onPress={() => {
+                useAuthStore.persist.clearStorage();
+                navigation.replace("Auth")
+              }}
+              className="bg-blue-600 px-6 py-3 rounded-lg"
+          >
+            <Text className="text-white text-center font-semibold text-lg">Выйти</Text>
+          </TouchableOpacity>
+        </View>
       </View>
   );
 };
-
-const styles = StyleSheet.create({
-  safeAreaWrapper: {
-    flex: 1,
-  },
-});
